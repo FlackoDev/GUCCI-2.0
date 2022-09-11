767f99877542def9bc8c7ff06e83e76b46ff3bfd
FlackoDev <83475271+FlackoDev@users.noreply.github.com>
2022-09-11 17:30:24 +0200

    Fixato comando ping

--- commands/ping.js
+++ commands/ping.js
@@ -5,8 +5,7 @@ module.exports = {
     .setName('ping')
     .setDescription('Esegue il ping!'),
   async execute(interaction) {
-    const ms = new Date(Date.now()).getDate() - interaction.createdAt.getDate()
-
-    await interaction.reply('Pong - ' + ms + "ms");
+    const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+    interaction.editReply(`Latenza: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
   },
 };
